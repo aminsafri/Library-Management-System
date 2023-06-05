ea01bbaee6f6db0c9e71852e6c1fb8cc49434342
amins <aminsafri02@gmail.com>
2023-06-05 19:34:52 +0800

    testing

---
+++ src/main/java/com/project/library/Test.java
@@ -0,0 +1,10 @@
+package com.project.library;
+
+public class Test {
+    public static void main(String[] args){
+
+        System.out.println("Hello world");
+        System.out.println("Hello world MF");
+
+    }
+}
