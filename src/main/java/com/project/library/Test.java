98b2a1bbf6bc4943ec5cf2e82360e60b3aa106bd
Fahmi <fahmi.izuddin2200@gmail.com>
2023-06-05 21:40:45 +0800

    testing

--- src/main/java/com/project/library/Test.java
+++ src/main/java/com/project/library/Test.java
@@ -4,10 +4,8 @@ public class Test {
     public static void main(String[] args){
 
         System.out.println("Hello world");
-<<<<<<< HEAD
         System.out.println("Hello world MF");
 
-=======
->>>>>>> origin/master
+
     }
 }
