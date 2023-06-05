e2bba982e8f9740e85ac36764d83cdea28009533
aiman <aimanrifq@gmail.com>
2023-06-05 21:46:04 +0800

    Aiman edit v2

--- src/main/java/com/project/library/Test.java
+++ src/main/java/com/project/library/Test.java
@@ -6,6 +6,7 @@ public class Test {
         System.out.println("Hello world");
         System.out.println("Fahmi");
         System.out.println("Hello world MF");
+        System.out.println("Hello world");
 
 
 
