2aaf281cc9e6304393ead566a4fa8c70ebce0762
amins <aminsafri02@gmail.com>
2023-06-05 22:23:26 +0800

    testing

--- src/main/java/com/project/library/Test.java
+++ src/main/java/com/project/library/Test.java
@@ -4,13 +4,5 @@ public class Test {
     public static void main(String[] args){
 
         System.out.println("Hello world");
-        System.out.println("Fahmi");
-        System.out.println("Hello world MF");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello");
-
-
-
     }
 }
