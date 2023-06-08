9202ec703f1e3f95300aac3eadb64379336df157
Fahmi <fahmi.izuddin2200@gmail.com>
2023-06-08 22:45:00 +0800

    Fahmi

---
+++ src/main/java/com/project/library/model/Book.java
@@ -0,0 +1,60 @@
+package com.project.library.model;
+import jakarta.persistence.*;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_id")
+    private Integer bookId;
+
+    @Column(name = "book_name")
+    private String bookName;
+
+    @Column(name = "author")
+    private String author;
+
+    public Book() {
+
+    }
+
+    public Book(Integer bookId, String bookName, String author) {
+        this.bookId = bookId;
+        this.bookName = bookName;
+        this.author = author;
+    }
+
+    public Integer getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Integer bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookId=" + bookId +
+                ", bookName='" + bookName + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
