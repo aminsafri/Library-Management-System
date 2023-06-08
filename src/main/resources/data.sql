a1923993f8de886defb6b2225458711b68c87916
Fahmi <fahmi.izuddin2200@gmail.com>
2023-06-08 23:11:04 +0800

    Miaw

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into book(book_name, author) values ('Legends of Archeus', 'Adam');
+insert into book(book_name, author) values ('Tears of Kingdom', 'Gary');
+insert into book(book_name, author) values ('Summer Time', 'Gray');
+insert into book(book_name, author) values ('Fanatix', 'Mary');
