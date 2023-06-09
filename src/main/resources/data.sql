90261e8d58aec315da774dd7f924f106ba156784
amins <aminsafri02@gmail.com>
2023-06-09 17:34:58 +0800

    section sql v1

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,35 @@ insert into book(book_name, author) values ('Legends of Archeus', 'Adam');
 insert into book(book_name, author) values ('Tears of Kingdom', 'Gary');
 insert into book(book_name, author) values ('Summer Time', 'Gray');
 insert into book(book_name, author) values ('Fanatix', 'Mary');
+
+INSERT INTO section(section_id, section_name, description, location)
+VALUES (1, 'Fiction', 'This section contains a variety of fictional books.', 'First Floor');
+
+INSERT INTO section(section_id, section_name, description, location)
+VALUES (2, 'Non-fiction', 'This section contains non-fictional books including biographies, history, etc.', 'Second Floor');
+
+INSERT INTO section(section_id, section_name, description, location)
+VALUES (3, 'Children', 'This section is for children books covering a range of topics suitable for kids.', 'Ground Floor');
+
+INSERT INTO section(section_id, section_name, description, location)
+VALUES (4, 'Sci-fi & Fantasy', 'Books in this section are all about science fiction and fantasy genres.', 'First Floor');
+
+INSERT INTO section(section_id, section_name, description, location)
+VALUES (5, 'Computer Science', 'This section contains books on computer science and programming.', 'Third Floor');
+
+INSERT INTO section(section_id, section_name, description, location)
+VALUES (6, 'Romance', 'This section contains romance novels, from historical romance to contemporary works.', 'Second Floor');
+
+INSERT INTO section(section_id, section_name, description, location)
+VALUES (7, 'Mystery & Thriller', 'Books in this section will keep you on the edge of your seat with suspense, thrillers, and mystery novels.', 'Third Floor');
+
+INSERT INTO section(section_id, section_name, description, location)
+VALUES (8, 'Business & Finance', 'This section contains books related to business, finance, and economics.', 'Fourth Floor');
+
+INSERT INTO section(section_id, section_name, description, location)
+VALUES (9, 'Self-help', 'Self-help books including personal development, motivation, and psychology can be found in this section.', 'First Floor');
+
+INSERT INTO section(section_id, section_name, description, location)
+VALUES (10, 'Biography & Autobiography', 'Read about the lives of fascinating people in this section containing biographies and autobiographies.', 'Second Floor');
+
+
