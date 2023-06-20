c86c937ef8f612709637a06ccea5823d679a025c
Fahmi <fahmi.izuddin2200@gmail.com>
2023-06-20 17:08:06 +0800

    Miaw22

--- src/main/java/com/project/library/controller/BookController.java
+++ src/main/java/com/project/library/controller/BookController.java
@@ -1,25 +1,111 @@
-//package com.project.library.controller;
-//
-//import com.project.library.repository.BookRepository;
-//import com.project.library.repository.CopyRepository;
-//import com.project.library.repository.SectionRepository;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//@RequestMapping("/books")
-//public class BookController {
-//
-//    private final BookRepository bookRepository;
-//    private final SectionRepository sectionRepository;
-//    private final CopyRepository copyRepository;
-//
-//    public BookController(BookRepository bookRepository, SectionRepository sectionRepository, CopyRepository copyRepository) {
-//        this.bookRepository = bookRepository;
-//        this.sectionRepository = sectionRepository;
-//        this.copyRepository = copyRepository;
-//    }
-//
-//    // All the book-related methods you have in the original controller go here
-//    // ...
-//}
+package com.project.library.controller;
+
+import com.project.library.model.Book;
+import com.project.library.model.Copy;
+import com.project.library.repository.BookRepository;
+import com.project.library.repository.CopyRepository;
+import com.project.library.repository.SectionRepository;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/books")
+public class BookController {
+
+    private final BookRepository bookRepository;
+    private final SectionRepository sectionRepository;
+    private final CopyRepository copyRepository;
+
+    public BookController(BookRepository bookRepository, SectionRepository sectionRepository, CopyRepository copyRepository) {
+        this.bookRepository = bookRepository;
+        this.sectionRepository = sectionRepository;
+        this.copyRepository = copyRepository;
+    }
+
+    @GetMapping("list")
+    public String showUpdateForm(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "list-book";
+    }
+
+    @GetMapping("signup")
+    public String showSignUpForm(Book book, Model model){
+        model.addAttribute("sections", sectionRepository.findAll());
+        return "add-book";
+    }
+
+    @PostMapping("add")
+    public String addBook(@Valid Book book, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "add-book";
+        }
+
+        bookRepository.save(book);
+        return "redirect:list";
+    }
+
+    @GetMapping("update")
+    public String showUpdateMainForm(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "choose-book-to-update";
+    }
+
+
+    @GetMapping("edit/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        Book book = bookRepository.findById((int) id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid book Id:" + id));
+        model.addAttribute("book", book);
+        model.addAttribute("sections", sectionRepository.findAll());
+        return "update-book";
+    }
+
+
+    @PostMapping("update/{id}")
+    public String updateBook(@PathVariable("id") long id, @Valid Book book, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            book.setBookId((int) id);
+            return "index";
+        }
+
+        model.addAttribute("books", bookRepository.findAll());
+        bookRepository.save(book);
+        return "list-book";
+    }
+
+    @GetMapping("delete")
+    public String showDeleteMainForm(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "choose-book-to-delete";
+    }
+
+
+    @GetMapping("delete/{id}")
+    public String deleteBook(@PathVariable("id") long id, Model model) {
+        Book book = bookRepository.findById((int) id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid book Id:" + id));
+
+        List<Copy> copy = book.getCopy(); // Get the books associated with the borrower
+
+        // Remove the borrower from the books
+        for (Copy copies : copy) {
+            copies.setBorrower(null);
+            copyRepository.save(copies);
+        }
+
+        bookRepository.delete(book);
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "list-book";
+    }
+
+
+}
--- src/main/java/com/project/library/controller/LibraryController.java
+++ src/main/java/com/project/library/controller/LibraryController.java
@@ -6,10 +6,7 @@ import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,82 +33,82 @@ public class LibraryController {
         this.employeeRepository = employeeRepository;
     }
 
-    @GetMapping("list")
-    public String showUpdateForm(Model model) {
-        model.addAttribute("books", bookRepository.findAll());
-        return "list-book";
-    }
-
-    @GetMapping("signup")
-    public String showSignUpForm(Book book, Model model){
-        model.addAttribute("sections", sectionRepository.findAll());
-        return "add-book";
-    }
-
-    @PostMapping("add")
-    public String addBook(@Valid Book book, BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            return "add-book";
-        }
-
-        bookRepository.save(book);
-        return "redirect:list";
-    }
-
-    @GetMapping("update")
-    public String showUpdateMainForm(Model model) {
-        model.addAttribute("books", bookRepository.findAll());
-        return "choose-book-to-update";
-    }
-
-
-    @GetMapping("edit/{id}")
-    public String showUpdateForm(@PathVariable("id") long id, Model model) {
-        Book book = bookRepository.findById((int) id)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid book Id:" + id));
-        model.addAttribute("book", book);
-        model.addAttribute("sections", sectionRepository.findAll());
-        return "update-book";
-    }
-
-
-    @PostMapping("update/{id}")
-    public String updateBook(@PathVariable("id") long id, @Valid Book book, BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            book.setBookId((int) id);
-            return "index";
-        }
-
-        model.addAttribute("books", bookRepository.findAll());
-        bookRepository.save(book);
-        return "list-book";
-    }
-
-    @GetMapping("delete")
-    public String showDeleteMainForm(Model model) {
-        model.addAttribute("books", bookRepository.findAll());
-        return "choose-book-to-delete";
-    }
-
-
-    @GetMapping("delete/{id}")
-    public String deleteBook(@PathVariable("id") long id, Model model) {
-        Book book = bookRepository.findById((int) id)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid book Id:" + id));
-
-        List<Copy> copy = book.getCopy(); // Get the books associated with the borrower
-
-        // Rem1ove the borrower from the books
-        for (Copy copies : copy) {
-            copies.setBorrower(null);
-            copyRepository.save(copies);
-        }
-
-        bookRepository.delete(book);
-        model.addAttribute("books", bookRepository.findAll());
-
-        return "list-book";
-    }
+//    @GetMapping("list")
+//    public String showUpdateForm(Model model) {
+//        model.addAttribute("books", bookRepository.findAll());
+//        return "list-book";
+//    }
+//
+//    @GetMapping("signup")
+//    public String showSignUpForm(Book book, Model model){
+//        model.addAttribute("sections", sectionRepository.findAll());
+//        return "add-book";
+//    }
+//
+//    @PostMapping("add")
+//    public String addBook(@Valid Book book, BindingResult result, Model model) {
+//        if (result.hasErrors()) {
+//            return "add-book";
+//        }
+//
+//        bookRepository.save(book);
+//        return "redirect:list";
+//    }
+//
+//    @GetMapping("update")
+//    public String showUpdateMainForm(Model model) {
+//        model.addAttribute("books", bookRepository.findAll());
+//        return "choose-book-to-update";
+//    }
+//
+//
+//    @GetMapping("edit/{id}")
+//    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+//        Book book = bookRepository.findById((int) id)
+//                .orElseThrow(() -> new IllegalArgumentException("Invalid book Id:" + id));
+//        model.addAttribute("book", book);
+//        model.addAttribute("sections", sectionRepository.findAll());
+//        return "update-book";
+//    }
+//
+//
+//    @PostMapping("update/{id}")
+//    public String updateBook(@PathVariable("id") long id, @Valid Book book, BindingResult result, Model model) {
+//        if (result.hasErrors()) {
+//            book.setBookId((int) id);
+//            return "index";
+//        }
+//
+//        model.addAttribute("books", bookRepository.findAll());
+//        bookRepository.save(book);
+//        return "list-book";
+//    }
+//
+//    @GetMapping("delete")
+//    public String showDeleteMainForm(Model model) {
+//        model.addAttribute("books", bookRepository.findAll());
+//        return "choose-book-to-delete";
+//    }
+//
+//
+//    @GetMapping("delete/{id}")
+//    public String deleteBook(@PathVariable("id") long id, Model model) {
+//        Book book = bookRepository.findById((int) id)
+//                .orElseThrow(() -> new IllegalArgumentException("Invalid book Id:" + id));
+//
+//        List<Copy> copy = book.getCopy(); // Get the books associated with the borrower
+//
+//        // Rem1ove the borrower from the books
+//        for (Copy copies : copy) {
+//            copies.setBorrower(null);
+//            copyRepository.save(copies);
+//        }
+//
+//        bookRepository.delete(book);
+//        model.addAttribute("books", bookRepository.findAll());
+//
+//        return "list-book";
+//    }
 
     //borrower
 
@@ -309,7 +306,7 @@ public class LibraryController {
 
 
     @PostMapping("updatecopy/{id}")
-    public String updateCopy(@PathVariable("id") long id, @Valid Copy copy, BindingResult result, Model model) {
+    public String updateCopy(@PathVariable("id") long id, @Valid Copy copy, @RequestParam String startDate, BindingResult result, Model model) {
         if (result.hasErrors()) {
             copy.setCopyId((int) id);
             return "update-copy";
